da85fd6ed1f822eca05c9e57ff8f578a39841cde
Filipo Negrao <filipo@accountfy.com>
2019-09-19 22:45:18 -0300

    criacao do projeto

---
+++ router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/filiponegrao/desafio-globo.com/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize(r *gin.Engine) {
+	api := r.Group("")
+	{
+		//Auto Generate
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"github.com/filiponegrao/desafio-globo.com/middleware"
+	"github.com/filiponegrao/desafio-globo.com/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+func Setup(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+	r.Use(middleware.SetDBtoContext(db))
+	router.Initialize(r)
+	return r
+}
