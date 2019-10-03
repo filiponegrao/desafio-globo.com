2edf9c82e2be7c29f057784be0caa6ee0daaf6d2
Filipo Negrao <filipo@accountfy.com>
2019-10-03 20:58:29 -0300

    mudancas para recuperacao de senha pt2

--- controllers/root.go
+++ controllers/root.go
@@ -18,10 +18,12 @@ func APIEndpoints(c *gin.Context) {
 	baseURL := fmt.Sprintf("%s://%s", reqScheme, reqHost)
 
 	resources := map[string]string{
-		"bookmarks_url": baseURL + "/bookmarks",
-		"bookmark_url":  baseURL + "/bookmarks/{id}",
-		"users_url":     baseURL + "/users",
-		"user_url":      baseURL + "/users/{id}",
+		"bookmarks_url":         baseURL + "/bookmarks",
+		"bookmark_url":          baseURL + "/bookmarks/{id}",
+		"password_recovers_url": baseURL + "/password_recovers",
+		"password_recover_url":  baseURL + "/password_recovers/{id}",
+		"users_url":             baseURL + "/users",
+		"user_url":              baseURL + "/users/{id}",
 	}
 
 	c.IndentedJSON(http.StatusOK, resources)
